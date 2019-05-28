de7afbee456b824567327e258d458b58cf8ddaa5
Diggelmann Tom, GHR-SRT-NEX-C-1 <tom.diggelmann@swisscom.com>
2019-05-28 15:27:58 +0200

    Started styling input fields

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
+
 body, #app {
   padding: 0;
   margin: 0;
+
+  font-family: 'Nunito', 'Trebuchet MS', 'Arial', sans-serif;
+  font-size: 18px;
 }
 #app {
   min-height: 100vh;
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="input-field">
+  <div :class="{ 'input-field': true, 'active': localValue }">
     <label :for="name">{{ label }}</label>
     <input v-model="localValue" :type="type" :name="name" :placeholder="label" />
   </div>
@@ -26,3 +26,45 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.input-field {
+  position: relative;
+
+  input {
+    padding: 4px 12px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+
+    outline: none;
+    border: 2px solid #eee;
+    border-radius: 24px;
+    background-color: #eee;
+
+    font-family: inherit;
+    font-size: inherit;
+    color: black;
+
+    &:active, &:focus {
+      outline: none;
+      background-color: white;
+    }
+  }
+
+  label {
+    position: absolute;
+    z-index: -1;
+    top: 50%;
+    left: 10px;
+    transform: translate(0, -50%);
+    transition: transform .1s ease-in-out, left .1s ease-in-out;
+  }
+
+  &.active {
+    label {
+      left: 0px;
+      transform: translate(calc(-100% - 10px), -50%);
+    }
+  }
+}
+</style>
