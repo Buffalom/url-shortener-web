c1f0c7d3b728425fd878fa99b379a2d4fa4dbe1d
Tom Diggelmann <mail@tomdgm.ch>
2019-06-02 00:34:23 +0200

    Removed console.log

--- src/utils/globalErrorHandler.js
+++ src/utils/globalErrorHandler.js
@@ -1,6 +1,5 @@
 export default function (self) {
   return function (error) {
-    console.dir(error.response)
     if (error.response.status === 403) {
       self.$router.push({
         name: 'login',
